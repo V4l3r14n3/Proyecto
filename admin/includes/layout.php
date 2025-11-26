e71edc01e5b638e96cfdb819ea217bffe16b04ab
V4l3r14n3 <vale.mantilla15@gmail.com>
2025-11-26 10:51:22 -0500

    admin

--- admin/includes/layout.php
+++ admin/includes/layout.php
@@ -3,7 +3,7 @@ session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . "/Proyecto/includes/rutas.php";
 
 // Bloquear acceso si no es admin
-if (!isset($_SESSION['usuario']) || $_SESSION['usuario']['rol'] !== 'Administrador') {
+if (!isset($_SESSION['usuario']) || $_SESSION['usuario']['rol'] !== 'admin') {
     header("Location: " . BASE_URL . "pages/login.php");
     exit();
 }
